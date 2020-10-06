3348e5ab751381288f36713ae33ef553a026bfca
Nick Lemmon <nick.lemmon@gmail.com>
2020-10-06 16:03:21 -0400

    Fix navigation

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,76 +1,43 @@
-import React from 'react'
-import { PageLink } from 'components/links'
+import React, { useEffect, useState } from 'react'
+import FocusLock from 'react-focus-lock'
 import classNames from 'classnames'
+import { PageLink } from 'components/links'
 import ScreenReaderOnly from 'components/ScreenReaderOnly'
 import styles from './Navigation.module.css'
 
-export default class Navigation extends React.Component {
-  constructor(props) {
-    super(props)
+export default function Navigation({ className, theme }) {
+  const [isOpen, setIsOpen] = useState(false)
 
-    this.state = {
-      isOpen: false,
-    }
-
-    this.handleClick = this.handleClick.bind(this)
-    this.handleStartKeypress = this.handleStartKeypress.bind(this)
-    this.handleEndKeypress = this.handleEndKeypress.bind(this)
-
-    this.firstFocusable = React.createRef()
-    this.lastFocusable = React.createRef()
-  }
-
-  handleStartKeypress(e) {
-    if (e.shiftKey && e.keyCode === 9 && this.state.isOpen) {
-      e.preventDefault()
-
-      this.lastFocusable.focus()
-    }
-  }
-
-  handleEndKeypress(e) {
-    if (!e.shiftKey && e.keyCode === 9) {
-      e.preventDefault()
-
-      this.firstFocusable.current.focus()
-    }
-  }
-
-  handleClick(e) {
-    e.preventDefault
-
+  useEffect(() => {
     // DOM manipulation! 😭😭😭
-    if (this.state.isOpen === false) {
-      document.querySelector('html').setAttribute('style', 'overflow: hidden;')
-    } else {
-      document.querySelector('html').setAttribute('style', 'overflow: initial;')
+    // This could be refactored to use context, or state could be lifted in some other way,
+    // however, this addresses preventing background scrolling in the most simple way possible.
+    if (isOpen) {
+      return document
+        .querySelector('html')
+        .setAttribute('style', 'overflow: hidden;')
     }
 
-    this.setState((prevState) => ({
-      isOpen: !prevState.isOpen,
-    }))
-  }
+    return document
+      .querySelector('html')
+      .setAttribute('style', 'overflow: initial;')
+  }, [isOpen])
 
-  render() {
-    const { className, theme } = this.props
-    const { isOpen } = this.state
-
-    return (
-      <div
-        className={classNames(
-          styles.Navigation,
-          theme === 'light' && styles.LightTheme,
-          theme === 'dark' && styles.DarkTheme,
-          className
-        )}
-      >
+  return (
+    <div
+      className={classNames(
+        styles.Navigation,
+        theme === 'light' && styles.LightTheme,
+        theme === 'dark' && styles.DarkTheme,
+        className
+      )}
+    >
+      <FocusLock disabled={!isOpen}>
         <button
           className={classNames(styles.Button, isOpen ? styles.isOpen : '')}
           aria-expanded={isOpen}
           aria-controls="nav-menu"
-          onClick={this.handleClick}
-          onKeyDown={(e) => this.handleStartKeypress(e)}
-          ref={this.firstFocusable}
+          onClick={() => setIsOpen(!isOpen)}
           data-cy="navigation-button"
         >
           <ScreenReaderOnly>{isOpen ? 'Open' : 'Close'} Menu</ScreenReaderOnly>
@@ -92,45 +59,24 @@ export default class Navigation extends React.Component {
             isOpen ? styles.isOpen : styles.isClosed
           )}
           id="nav-menu"
-          data-cy="navigation-menu"
         >
-          <PageLink className={styles.Link} href="/" onClick={this.handleClick}>
+          <PageLink className={styles.Link} href="/">
             Home
           </PageLink>
 
-          <PageLink
-            className={styles.Link}
-            href="/archives"
-            onClick={this.handleClick}
-          >
+          <PageLink className={styles.Link} href="/archives">
             Archives
           </PageLink>
 
-          <PageLink
-            className={styles.Link}
-            href="/about"
-            onClick={this.handleClick}
-            onKeyDown={(e) => this.handleEndKeypress(e)}
-            innerRef={(el) => {
-              this.lastFocusable = el
-            }}
-          >
+          <PageLink className={styles.Link} href="/about">
             About
           </PageLink>
 
-          <PageLink
-            className={styles.Link}
-            href="/resume"
-            onClick={this.handleClick}
-            onKeyDown={(e) => this.handleEndKeypress(e)}
-            innerRef={(el) => {
-              this.lastFocusable = el
-            }}
-          >
+          <PageLink className={styles.Link} href="/resume">
             Resume
           </PageLink>
         </nav>
-      </div>
-    )
-  }
+      </FocusLock>
+    </div>
+  )
 }
