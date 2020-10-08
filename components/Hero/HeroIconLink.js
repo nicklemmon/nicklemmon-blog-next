6ee9844be43158057cb0f5d478e54966597e8ffc
Nick Lemmon <nick.lemmon@gmail.com>
2020-10-07 22:31:40 -0400

    Remove remaining class component

--- components/Hero/HeroIconLink.js
+++ components/Hero/HeroIconLink.js
@@ -3,29 +3,23 @@ import { Icon } from 'react-icons-kit'
 import ScreenReaderOnly from 'components/ScreenReaderOnly'
 import styles from './Hero.module.css'
 
-export default class HeroLinkIcon extends React.Component {
-  render() {
-    const { icon, href, screenReaderContent } = this.props
+export default function HeroIconLink(props) {
+  const { icon, href, screenReaderContent } = props
 
-    return (
-      <a
-        className={styles.IconLink}
-        href={href}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Icon
-          className={styles.Icon}
-          size={'calc(2.5rem + 1.5vw)'}
-          icon={icon}
-        />
+  return (
+    <a
+      className={styles.IconLink}
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Icon className={styles.Icon} size={'calc(2.5rem + 1.5vw)'} icon={icon} />
 
-        <ScreenReaderOnly>
-          {screenReaderContent} (Opens in a new tab)
-        </ScreenReaderOnly>
+      <ScreenReaderOnly>
+        {screenReaderContent} (Opens in a new tab)
+      </ScreenReaderOnly>
 
-        <span className={styles.IconUnderlay} role="presentation" />
-      </a>
-    )
-  }
+      <span className={styles.IconUnderlay} role="presentation" />
+    </a>
+  )
 }
