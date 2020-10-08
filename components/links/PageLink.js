94248df43788665d81f80feddd1e3250e83d7243
Nick Lemmon <nick.lemmon@gmail.com>
2020-10-08 16:00:19 -0400

    Navigation closes when links are clicked

--- components/links/PageLink.js
+++ components/links/PageLink.js
@@ -5,8 +5,8 @@ export default function PageLink(props) {
   const { onClick, onKeyDown, children, href, className } = props
 
   return (
-    <Link href={href} passHref onKeyDown={onKeyDown} onClick={onClick}>
-      <a className={className} data-id={props['data-id']}>
+    <Link href={href} passHref onKeyDown={onKeyDown}>
+      <a className={className} data-id={props['data-id']} onClick={onClick}>
         {children}
       </a>
     </Link>
