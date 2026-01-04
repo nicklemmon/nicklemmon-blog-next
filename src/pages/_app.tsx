2b0e887da90059755695bebd7e452be5ff80ded7
Nick Lemmon <nick.lemmon@gmail.com>
2026-01-04 06:10:30 -0500

    Improves OG image (#926)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,31 @@ import { Analytics } from '@vercel/analytics/react'
 import '../styles/global.css'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const defaultTitle = 'Nick Lemmon, Front End Developer'
+  const defaultDescription = 'A frontend engineering leader driven to create accessible user interfaces at scale.'
+  const ogImageUrl = `${process.env.NEXT_PUBLIC_WEBSITE_URL}/api/og?title=${encodeURIComponent(defaultTitle)}`
+
   return (
     <>
       <Head>
-        <title>Nick Lemmon, Front End Developer</title>
+        <title>{defaultTitle}</title>
+
+        {/* Open Graph */}
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={defaultTitle} />
+        <meta property="og:description" content={defaultDescription} />
+        <meta property="og:image" content={ogImageUrl} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+
+        {/* Twitter Card */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={defaultTitle} />
+        <meta name="twitter:description" content={defaultDescription} />
+        <meta name="twitter:image" content={ogImageUrl} />
+
+        {/* Standard meta */}
+        <meta name="description" content={defaultDescription} />
       </Head>
 
       <Component {...pageProps} />
