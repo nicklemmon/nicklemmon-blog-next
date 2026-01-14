35f87d6873c03aefe250569b1c34e83ea9a2e2d2
Nick Lemmon <nick.lemmon@gmail.com>
2026-01-13 20:08:28 -0500

    WIP

--- cypress/tests/accessibility.cy.js
+++ cypress/tests/accessibility.cy.js
@@ -1,30 +1,31 @@
 describe('Accessibility', () => {
-  beforeEach(() => {
-    cy.injectAxe()
-  })
-
   it('has no detectable accessibility violations on the index page', () => {
     cy.visit('/')
+    cy.injectAxe()
     cy.checkA11y()
   })
 
   it('has no detectable accessibility violations on the about page', () => {
     cy.visit('/about')
+    cy.injectAxe()
     cy.checkA11y()
   })
 
   it('has no detectable accessibility violations on the archives page', () => {
     cy.visit('/archives')
+    cy.injectAxe()
     cy.checkA11y()
   })
 
   it('has no detectable accessibility violations on the resume page', () => {
     cy.visit('/resume')
+    cy.injectAxe()
     cy.checkA11y()
   })
 
   it('has no detectable accessibility violations on the 404 page', () => {
     cy.visit('/does-not-exist', { failOnStatusCode: false })
+    cy.injectAxe()
     cy.checkA11y()
   })
 
@@ -35,7 +36,8 @@ describe('Accessibility', () => {
     // Click on the first article card to navigate to a post
     cy.get('[data-id="article-card"]').first().click()
 
-    // Check accessibility on the blog post page
+    // Inject axe after navigation
+    cy.injectAxe()
     cy.checkA11y()
   })
 })
